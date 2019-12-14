c04ae2c367af5e2e9a5256cb292d8d2d4e9c0cb8
gustavjunedahl <gustav.junedahl@gmail.com>
2019-12-14 16:41:22 +0100

    Solved day 12 part 2

--- Day 12/Moons.js
+++ Day 12/Moons.js
@@ -9,6 +9,7 @@ const moons = fs
     let z = Number(s[2].substring(s[2].indexOf('=') + 1, s[2].length - 1));
     return { x, y, z };
   });
+let velocities = moons.map(() => ({ x: 0, y: 0, z: 0 }));
 
 const applyVelocity = (moons, velocities) => {
   moons.forEach((moon, i) => {
@@ -19,21 +20,18 @@ const applyVelocity = (moons, velocities) => {
 const applyGravity = (moons, velocities) => {
   moons.forEach((moon, index) => {
     Object.keys(moon).forEach(key => {
-      moons.forEach((innerMoon, innerIndex) => {
-        if (innerIndex !== index) {
-          if (innerMoon[key] > moon[key]) {
-            velocities[index][key]++;
-          } else if (innerMoon[key] < moon[key]) {
-            velocities[index][key]--;
-          }
+      moons.forEach(innerMoon => {
+        if (innerMoon[key] > moon[key]) {
+          velocities[index][key]++;
+        } else if (innerMoon[key] < moon[key]) {
+          velocities[index][key]--;
         }
       });
     });
   });
 };
 
-const main = () => {
-  let velocities = moons.map(() => ({ x: 0, y: 0, z: 0 }));
+const part1 = () => {
   for (let loops = 0; loops < 1000; loops++) {
     applyGravity(moons, velocities);
     applyVelocity(moons, velocities);
@@ -52,4 +50,40 @@ const main = () => {
   console.log(tot);
 };
 
-main();
+const gcd = (a, b) => {
+  var t = 0;
+  a < b && ((t = b), (b = a), (a = t)); // swap them if a < b
+  t = a % b;
+  return t ? gcd(b, t) : b;
+};
+
+const lcm = (a, b) => (a / gcd(a, b)) * b;
+
+const part2 = () => {
+  const initPos = JSON.parse(JSON.stringify(moons));
+  const initVel = JSON.parse(JSON.stringify(velocities));
+  let intervals = {};
+  let steps = 0;
+  while (Object.keys(intervals).length < 3) {
+    applyGravity(moons, velocities);
+    applyVelocity(moons, velocities);
+    steps++;
+    Object.keys(moons[0]).forEach(key => {
+      let repeat = true;
+      for (let i = 0; i < moons.length; i++) {
+        repeat =
+          repeat &&
+          moons[i][key] === initPos[i][key] &&
+          velocities[i][key] === initVel[i][key];
+      }
+      if (repeat && intervals[key] === undefined) {
+        intervals[key] = steps;
+      }
+    });
+  }
+  let xy = lcm(intervals.x, intervals.y);
+  console.log(lcm(xy, intervals.z));
+};
+
+module.exports.part1 = part1;
+module.exports.part2 = part2;
